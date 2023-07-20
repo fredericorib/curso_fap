cc274e8495fb5c1d677e3cf9f3a632b31a71a2ef
fredericorib <139914295+fredericorib@users.noreply.github.com>
2023-07-20 19:45:17 +0000

    atividade condicionais

--- aula_0720_exercicios.js
+++ aula_0720_exercicios.js
@@ -20,3 +20,59 @@ function divisao(num1,num2){
 }
 console.log(divisao(10,2))
 
+var num3 = 43245
+if (num3 > 0){
+    console.log(true);
+}
+
+if (num3%2 == 0){
+    console.log(`O ${num3} é par`);
+}
+
+string_vazia = ""
+if (string_vazia == ""){
+    console.log(`A string é vazia`);
+}
+
+if (num3 > 10){
+    console.log(`${num3} é maior que 10`)
+}
+var3 = true 
+if (typeof var3 == "boolean"){
+    console.log("A variável é boleana");
+}
+
+/*if else */
+var num3 = -43245
+if (num3 > 0){
+    console.log(`O número ${num3} é POSITIVO`);
+} else {
+    console.log(`ERROR: O número ${num3} não é POSITIVO`);
+}
+
+if (num3%2 == 0){
+    console.log(`O número ${num3} é PAR`);
+} else {
+    console.log(`O número ${num3} é IMPAR`);
+}
+
+string_vazia = "dgfdg"
+if (string_vazia == ""){
+    console.log(`A string é vazia`);
+} else{
+    console.log(`A string não é vazia, seu conteúdo é ${string_vazia}`);
+}
+
+if (num3 > 10){
+    console.log(`${num3} é maior que 10`)
+} else{
+    console.log(num3 *= 2)
+}
+
+var3 = 21 
+if (typeof var3 == "boolean"){
+    console.log("A variável é boleana");
+} else{
+    var3 = true
+    console.log("A variável não era booleana, por isso foi modificada para True")
+}
