7cb79cbc72ca9c21c5afe82d5744e90bf019b034
fredericorib <139914295+fredericorib@users.noreply.github.com>
2023-07-20 18:19:09 +0000

    Lista de exerciocios de sala

---
+++ aula_0720_exercicios.js
@@ -0,0 +1,22 @@
+var minhaVar;
+minhaVar = 10;
+
+var soma = 15+8;
+soma++;
+soma*=3;
+console.log("O valor da variável soma até agora é", soma);
+
+var fazSol = true;
+console.log(fazSol);
+
+var comida = ["arroz", "feijão", "ovo"];
+console.log(comida[1]);
+
+console.log(soma === minhaVar);
+console.log(minhaVar<=soma);
+
+function divisao(num1,num2){
+    return num1/num2;
+}
+console.log(divisao(10,2))
+
